1f7505e29dca1f6ae66389a125c688d9b50c7156
PatignierThomas <thomas.patignier@gmail.com>
2024-07-25 21:09:29 +0200

    added some error handling

--- src/Component/AddToCart.tsx
+++ src/Component/AddToCart.tsx
@@ -2,19 +2,32 @@ import React from 'react'
 import { CartProps, Product } from './Cart.tsx'
 
 function AddToCart({setProducts}: Pick<CartProps, "setProducts">) {
+    const [error, setError] = React.useState<string | null>(null)
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        const form = e.currentTarget as HTMLFormElement
-        const name: string = (form[0] as HTMLInputElement).value
-        const price: number = Number((form[1] as HTMLInputElement).value)
-        setProducts((prevProduct: Product[]) => [...prevProduct, { id: (Math.floor(Math.random() * Date.now())) , name, price, quantity: 1 }])
+        const form = e.currentTarget as HTMLFormElement;
+        const name: string = (form[0] as HTMLInputElement).value;
+        const price: number = Number((form[1] as HTMLInputElement).value);
+        if (!name || !price) {
+            setError('Veuillez remplir tous les champs.');
+            return
+        }
+        setProducts((prevProduct: Product[]) => {
+            const productExists = prevProduct.some(product => product.name === name);
+            if (productExists) {
+                setError('Un produit avec ce nom existe déjà.');
+                return prevProduct;
+            }
+            return [...prevProduct, { id: (Math.floor(Math.random() * Date.now())) , name, price, quantity: 1 }]
+        })
     }
 
   return (
     <>
         <h2 className="addProduct">Ajouter un produit </h2>
         <form className="addProductForm" onSubmit={handleSubmit}>
+            {error && <p className='error'>{error}</p>}
             <input type="text" placeholder="Product name"/>
             <input type="number" placeholder="Price"/>
             <button type="submit">Ajouter</button>
